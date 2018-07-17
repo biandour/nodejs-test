843f00e147b935bccf2ec6021dc984759906ecf6
biandour <biandour@qq.com>
2018-07-17 18:18:03 +0800

    update event-lessons

--- event-lesson.js
+++ event-lesson.js
@@ -59,25 +59,66 @@ const eventEmitter = require('events').EventEmitter;
 //
 //myEvent.emit('plus', 222, 666);
 
-const util = require('util');
+//const util = require('util');
+//
+//class Pulse extends eventEmitter {
+//  constructor() {
+//    super()
+//  }
+//
+//  start() {
+//    let self = this;
+//    setInterval(function () {
+//      util.log('>>>>pulse');
+//      self.emit('pulse');
+//      util.log('<<<<pulse');
+//    }, 1000);
+//  }
+//}
+//
+//let pulse = new Pulse();
+//
+//pulse.on('pulse', function () {
+//  util.log('pulse received');
+//});
+//
+//pulse.start();
 
-class Pulse extends eventEmitter {
 
+let myEvent = new eventEmitter();
+
+function listener1() {
+  console.log('this is listener one');
 }
 
-Pulse.prototype.start = function () {
-  let self = this;
-  this.id = setInterval(function () {
-    util.log('>>>>pulse');
-    self.emit('pulse');
-    util.log('<<<<pulse');
-  }, 1000);
-};
+function listener2() {
+  console.log('this is listener two');
+}
 
-let pulse = new Pulse();
+function listener3() {
+  console.log('this is listener three');
+}
 
-pulse.on('pulse', function () {
-  util.log('pulse received');
-});
+myEvent.on('event', listener1);
+myEvent.on('event', listener2);
+myEvent.on('event', listener3);
+myEvent.emit('event');
 
-pulse.start();
+myEvent.removeListener('event', listener2); // 参数分别为注册的事件名，和事件上的回调函数
+myEvent.emit('event');
+
+myEvent.removeAllListeners('event'); // 若没有传参，则移除所有监听器
+myEvent.emit('event');
+
+/**
+超过10个监听时，不设置EventEmitter的最大监听数会提示：
+ (node) warning: possible EventEmitter memory leak detected. 11 listeners added.
+ Use emitter.setMaxListeners() to increase limit.
+ 设计者认为侦听器太多，可能导致内存泄漏，所以存在这样一个警告*/
+for (let i = 0; i < 12; i++) {
+  myEvent.on('event', function() {
+    console.log(`this is the ${i} listener`);
+  });
+}
+console.log(myEvent.listeners('event')); // 返回事件上注册的监听器列表
+
